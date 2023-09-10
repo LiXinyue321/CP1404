f70843cc9a52de2a80fc14169b016b3fdfb2bbb9
LiXinyue321 <140147846+LiXinyue321@users.noreply.github.com>
2023-09-10 21:31:40 +0800

    Add files via upload

---
+++ prac_08/dynamic_labels(1).py
@@ -0,0 +1,25 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Label
+
+
+NAMES = ("Bob Brown", "Cat Cyan", "Oren Ochre")
+
+
+class DynamicWidgetsApp(App):
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.main()
+        return self.root
+
+    def main(self):
+        """Create labels dynamically from list of names"""
+        for name in NAMES:
+            temp_label = Label(text=name)
+            self.root.ids.main.add_widget(temp_label)
+
+
+DynamicWidgetsApp().run()
