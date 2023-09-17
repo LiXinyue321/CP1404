176be12f7e1fea51fc39c763baff167d045e6539
LiXinyue321 <140147846+LiXinyue321@users.noreply.github.com>
2023-09-17 22:18:38 +0800

    Add files via upload

---
+++ prac_09/band.py
@@ -0,0 +1,25 @@
+"""
+CP1404 - Practical 9
+Band Class
+"""
+
+
+class Band:
+    """Band class"""
+
+    def __init__(self, name=""):
+        """Initialise band class instance"""
+        self.name = name
+        self.musicians = []
+
+    def __str__(self):
+        """Return a string representation of the band"""
+        return f"{self.name} ({', '.join([musician.__str__() for musician in self.musicians])})"
+
+    def add(self, musician):
+        """Add member to band"""
+        self.musicians.append(musician)
+
+    def play(self):
+        """Return a string showing the bands and the instruments they play"""
+        return '\n'.join([musician.play() for musician in self.musicians])
